f72df5c9203dc6c16b8973a461513ce5bc52b23c
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-31 12:55:53 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,17 +1,46 @@
-def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+require 'pry'
 
+
+def find_item_by_name_in_collection(name, collection)
+  #simulates adding items to your grocery cart
+  answer = nil
+  collection.each do |hashes|
+    #puts hashes
+    #binding pry
+    if name == hashes[:item]
+      answer = hashes
+    end
+  end
+  return answer
 end
+
+      
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-
+  #translate cart into new array of hashes including the :count 
+  new_cart = []
+  cart.each do |items_hash|
+    # puts items_hash
+    # binding pry
+    #items_array is giving full item hash
+    i = 0
+    found_item = find_item_by_name_in_collection(items_hash[:item], new_cart) 
+    while i < new_cart.length do
+      found_item = find_item_by_name_in_collection(items_hash[:item], new_cart)
+      if i == found_item
+        found_item[:count] += 1
+        binding.pry
+      else 
+        found_item[:count] = 1
+        new_cart << found_item
+      end
+      i =+ 1
+    end
+  end
+  return new_cart
 end
 
-
-  
+# found_item = find_item_by_name_in_collection(items_array[:item], new_cart)
+#         if found_item
+#           found_item[:count] += 1
+#           binding.pry
