b0441c69da2d18e7bbeb4677f910544cb51deaa3
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-31 13:02:49 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -14,17 +14,17 @@ end
 
 
 def consolidate_cart(cart)
-  #translate cart into new array of hashes including the :count
+  
+
+
+  
   new_cart = []
   cart.each do |items_hash|
-    # puts items_hash
-    # binding pry
-    #items_array is giving full item hash
     i = 0
     found_item = find_item_by_name_in_collection(items_hash[:item], new_cart)
     while i < new_cart.length do
       found_item = find_item_by_name_in_collection(items_hash[:item], new_cart)
-      if i == found_item
+      if found_item
         found_item[:count] += 1
         binding.pry
       else
